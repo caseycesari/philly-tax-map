74c2fba310dffcc0975d7367791766d2abe4d5c9
Casey Thomas <cpthomas@gmail.com>
2014-06-05 16:54:52 -0400

    Update filepaths to sql scripts

--- scripts/import_sql_data.sh
+++ scripts/import_sql_data.sh
@@ -7,11 +7,11 @@ echo "Enable postgis..."
 psql -d taxmap -c "CREATE EXTENSION postgis;"
 
 echo "Import OPA and parcel data"
-psql -d taxmap -f data/sql/create_opa_2014.sql
-psql -d taxmap -f data/sql/create_opa_2015.sql
-psql -d taxmap -f data/sql/import_opa_2014.sql
-psql -d taxmap -f data/sql/import_opa_2015.sql
-psql -d taxmap -f data/sql/import_parcels.sql
+psql -d taxmap -f create_opa_2014_table.sql
+psql -d taxmap -f create_opa_2015_table.sql
+psql -d taxmap -f import_opa_2014_data.sql
+psql -d taxmap -f import_opa_2015_data.sql
+psql -d taxmap -f create_and_import_parcels.sql
 
 echo "Create parcel map table..."
 psql -d taxmap -f create_tax_map_table.sql
