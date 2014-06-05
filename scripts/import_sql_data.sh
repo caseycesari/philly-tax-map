3d9ca635f36e8b5335c64b401073ddb39710e3a3
Casey Thomas <cpthomas@gmail.com>
2014-06-05 13:13:24 -0400

    Add status messages and parcel map table script

--- scripts/import_sql_data.sh
+++ scripts/import_sql_data.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
+echo "Create taxmap database..."
 createdb taxmap
+
+echo "Enable postgis..."
 psql -d taxmap -c "CREATE EXTENSION postgis;"
 
+echo "Import OPA and parcel data"
 psql -d taxmap -f data/sql/create_opa_2014.sql
 psql -d taxmap -f data/sql/create_opa_2015.sql
 psql -d taxmap -f data/sql/import_opa_2014.sql
 psql -d taxmap -f data/sql/import_opa_2015.sql
 psql -d taxmap -f data/sql/import_parcels.sql
+
+echo "Create parcel map table..."
+psql -d taxmap -f create_tax_map_table.sql
